a96905c960dad23f7af6f5ea8e3ab8270812949b
arpit0601 <66950445+arpit0601@users.noreply.github.com>
2026-07-05 10:44:10 +0530

    Initial Commit

---
+++ Workout-Diary/Workout_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_DiaryApp.swift
+//  Workout-Diary
+//
+//  Created by arpit on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Workout_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
