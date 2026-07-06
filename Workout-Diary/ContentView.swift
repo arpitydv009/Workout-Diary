43a72b55d7c884de23fd09727f83b326cfb50473
arpit0601 <66950445+arpit0601@users.noreply.github.com>
2026-07-06 13:47:05 +0530

    Completed basic Homescreen

--- Workout-Diary/ContentView.swift
+++ Workout-Diary/ContentView.swift
@@ -9,15 +9,75 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!   ")
+        NavigationView{
+            HStack {
+                VStack {
+                    
+                    HStack{
+                        
+                        
+                        Text("Workout-Diary")
+                            .italic()
+                            .font(Font.system(size: 31))
+                            .fontDesign(Font.Design.monospaced)
+                            .padding()
+                            .background(Color.cyan, in:RoundedRectangle(cornerRadius: 12))
+                            .frame(width: 300, height:75)
+                        
+                        
+                        //  .offset(x : 0, y:-300)
+                        
+                        
+                        Image(systemName: "dumbbell")
+                            .resizable()
+                            .foregroundStyle(Color.white)
+                            .frame(width: 50, height: 50)
+                        // .offset(x : 0, y:-300)
+                        
+                    }
+                    .frame(width: 300, height: 200, alignment:.top)
+                    .offset(x:0, y:-220)
+                    
+                    
+                    
+                    VStack {
+                        NavigationLink(destination: newWorkout()) {
+                            Text("New Workout Starter")
+                                .padding()
+                                .frame(width:400,height:50)
+                                .fontDesign(Font.Design.monospaced)
+                                .font(Font.system(size: 25))
+                                .foregroundStyle(Color.white)
+                                .italic()
+                                .background(Color.mint, in:RoundedRectangle(cornerRadius: 15))
+                        }
+                        
+                        NavigationLink(destination: testview()) {
+                            Text("History")
+                                .padding()
+                                .frame(width:400,height:50)
+                                .fontDesign(Font.Design.monospaced)
+                                .font(Font.system(size: 25))
+                                .foregroundStyle(Color.white)
+                                .italic()
+                                .background(Color.mint, in:RoundedRectangle(cornerRadius: 15))
+                        }
+                        
+                        
+                    }
+                    
+                    
+                    
+                }
+                .padding()
+                .preferredColorScheme(.dark)
+            }
+            
         }
-        .padding()
+        
     }
 }
+
 
 #Preview {
     ContentView()
