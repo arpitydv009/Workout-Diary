7da117cf47e173754fb13b859e335ff70bdcc8e8
arpit0601 <66950445+arpit0601@users.noreply.github.com>
2026-07-06 13:46:16 +0530

    Created Basic Homescreen along with Navigation Links

---
+++ Workout-Diary/newWorkout.swift
@@ -0,0 +1,18 @@
+//
+//  newWorkout.swift
+//  Workout-Diary
+//
+//  Created by arpit on 06/07/26.
+//
+
+import SwiftUI
+
+struct newWorkout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newWorkout()
+}
