02ad1fb8a17c01e9925a4e072233e08357698129
arpit0601 <66950445+arpit0601@users.noreply.github.com>
2026-07-20 15:10:11 +0530

    Implemented the table look into History

--- Workout-Diary/Code/History/pastSetViewer.swift
+++ Workout-Diary/Code/History/pastSetViewer.swift
@@ -12,14 +12,67 @@ struct pastSetViewer: View {
     @Bindable var exercise: exerciseModel
 
     var body: some View {
-        List(exercise.set_array, id: \.set) { name in
+        /*List(exercise.set_array, id: \.set) { name in
 
             DisclosureGroup("Set: \(name.set)") {
                 Text("Weight: \(name.weight)")
                 Text("Reps: \(name.reps)")
             }
 
+        }*/
+        
+        ScrollView{
+            
+            LazyVStack{
+                HStack{
+                    Text("Set")
+                        .frame(width: 80,height: 40)
+                        .background(Color.black, in:RoundedRectangle(cornerRadius: 12))
+                    
+                    Text("Weight")
+                        .frame(width: 120,height: 40)
+                        .background(Color.black, in:RoundedRectangle(cornerRadius: 12))
+
+                    Text("Reps")
+                        .frame(width: 80,height: 40)
+                        .background(Color.black, in:RoundedRectangle(cornerRadius: 12))
+
+                }
+                .bold()
+                .italic()
+                .font(.title2)
+              //  .font(Font.system(size: 18))
+                .foregroundStyle(Color.indigo)
+                .fontDesign(Font.Design.monospaced)
+               
+                
+                ForEach(exercise.set_array) {  name in
+                    
+                    HStack {
+                        Text("\(name.set)")
+                            .frame(width: 80)
+
+                        Text("\(name.weight)")
+                            .frame(width: 120)
+
+                        Text("\(name.reps)")
+                            .frame(width: 80)
+
+                    }
+                    .foregroundStyle(Color.mint)
+                    .fontDesign(Font.Design.rounded)
+                    .font(.title3)
+                    .italic()
+
+                    
+                }
+            }
+            .background(Color.black, in:RoundedRectangle(cornerRadius: 12))
+            
         }
+     //   .navigationTitle("Hello")
+        .font(Font.system(size: 20))
+        .controlSize(.large)
 
     }
 }
